ae89be1fdb01b6dcfdbc0a93831776f05f45c9df
EsaKris <esachris00@gmail.com>
2026-09-13 18:53:08 +0100

    modified:   src/config/site.ts

--- src/config/site.ts
+++ src/config/site.ts
@@ -31,8 +31,8 @@ export const site = {
     ],
     address: ['Old TETFund Building', 'Federal University of Petroleum Resources (FUPRE)'],
     website: {
-      display: 'fupre.edu.ng/center-for-safety-education',
-      href: 'https://fupre.edu.ng/center-for-safety-education',
+      display: 'fupre.edu.ng/',
+      href: 'https://fupre.edu.ng/',
     },
     /** Google Maps embed URL for the contact page. Leave empty until the location is confirmed. */
     mapEmbedUrl: '',
